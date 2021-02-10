acc31c398e539f46ad157cd2e01c5c2a53b8f2e1
Silvana <msilvanat@gmail.com>
2021-02-10 16:41:08 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Interactive console
+
+## Table of Contents
+Description -
+Technologies and resources -
+Requirements -
+Instructions - Installing -
+
+## Description
+
+This is an application that allows us to create, list, complete and delete tasks in the console. It also shows us the exact date the tasks were completed and allows us to have a better organization.
+
+##  Technologies and resources
+
+- Node.js
+- Nodemon
+
+### Requirements
+
+You need to have (installed):
+- [NodeJS](https://nodejs.org/)
+
+## Instructions - Installing
+
+A step by step series of examples that tell you how to get a development env running.
+
+1.- Clone the repository
+
+
+$ git clone https://github.com/msilvanat/Interactive-console-application.git
+
+
+
+2.- Install the following dependencies
+
+```
+npm i colors inquirer uuid
+
+```
+
+3.- To run the api, run this on the terminal
+```
+node app.js 
+```
+
+
+##  Author 
+- [@msilvanat](https://github.com/msilvanat)
+
---
+++ app.js
@@ -0,0 +1,75 @@
+require('colors'); //primero va la importacion de paquetes de terceros, luego las nuestras
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require('./helpers/inquirer');
+
+const Tareas = require('./models/tareas');
+
+
+const main = async () => {
+    let opt = '';
+
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if ( tareasDB ) { // CARGAR TAREAS
+        tareas.cargarTareasFromArray( tareasDB );
+    }
+
+    do {
+        //Imprimir el menu
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea( desc );
+                break;
+
+            case '2':
+                tareas.listadoCompleto();
+                break;
+
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+                break;
+
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+                break;
+
+                case '5':
+                    const ids = await mostrarListadoChecklist( tareas.listadoArr );
+                    tareas.toggleCompletadas( ids );
+                break;
+
+                case '6': // Borrar
+                const id = await listadoTareasBorrar( tareas.listadoArr );
+                if ( id !== '0' ) {
+                    const ok = await confirmar('¿Está seguro?');
+                    if ( ok ) {
+                        tareas.borrarTarea( id );
+                        console.log('Tarea borrada');
+                    }
+                }
+            break;
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        await pausa();
+
+
+    } while (opt !== '0');
+
+   
+}
+
+main();
